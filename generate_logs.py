b240a432fa66747923127c10edb901bc3593f21d
toast38coza <info@38.co.za>
2016-03-16 08:56:45 +0200

    A bunch of examples

--- generate_logs.py
+++ generate_logs.py
@@ -1,5 +1,61 @@
 import click, time, datetime, random
 
+states = ["Alabama",
+"Alaska",
+"Arizona",
+"Arkansas",
+"California",
+"Colorado",
+"Connecticut",
+"Delaware",
+"Florida",
+"Georgia",
+"Hawaii",
+"Idaho",
+"Illinois",
+"Indiana",
+"Iowa",
+"Kansas",
+"Kentucky",
+"Louisiana",
+"Maine",
+"Maryland",
+"Massachusetts",
+"Michigan",
+"Minnesota",
+"Mississippi",
+"Missouri",
+"Montana",
+"Nebraska",
+"Nevada",
+"New Hampshire",
+"New Jersey",
+"New Mexico",
+"New York",
+"North Carolina",
+"North Dakota",
+"Ohio",
+"Oklahoma",
+"Oregon",
+"Pennsylvania",
+"Rhode Island",
+"South Carolina",
+"South Dakota",
+"Tennessee",
+"Texas",
+"Utah",
+"Vermont",
+"Virginia",
+"Washington",
+"West Virginia",
+"Wisconsin",
+"Wyoming",
+"District of Columbia",
+"Puerto Rico",
+"Guam",
+"American Samoa",
+"U.S. Virgin Islands",
+"Northern Mariana Islands"]
 
 @click.command()
 @click.option('--count', default=1, help='Number of greetings.')
@@ -11,12 +67,12 @@ def generate_logs(count):
         dt = datetime.datetime.now().date().strftime("%m-%d-%Y")
         tm = time.strftime("%H:%M:%S")
         status = random.choice(["up", "up", "up", "up", "up", "up", "up", "up", "up", "down"])
-        province = random.choice(["The Eastern Cape", "The Free State", "Gauteng", "KwaZulu-Natal", "Limpopo", "Mpumalanga", "The Northern Cape", "North West"])
+        province = random.choice(states)
 
         date_time = "{} {}" . format (dt, tm)
     	message = "{} - {}: {}" . format (date_time, status, province)
     	
-    	with open("log/events.log", "a") as log_file:
+    	with open("logstash/inputs/events.log", "a") as log_file:
     		log_file.write("{}\n" . format(message))
     		wait_period = random.choice(range(0,10))
     		print(".")
@@ -25,3 +81,5 @@ def generate_logs(count):
 
 if __name__ == '__main__':
     generate_logs()
+
+
