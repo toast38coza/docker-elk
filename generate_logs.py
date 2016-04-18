56b7f1b72eac45bad0662847a9669b8eb77568c9
toast38coza <info@38.co.za>
2016-04-18 21:19:39 +0200

    plugin configuration for latest elasticsearch

--- generate_logs.py
+++ generate_logs.py
@@ -1,4 +1,4 @@
-import click, time, datetime, random
+import click, time, datetime, random, requests
 
 states = ["Alabama",
 "Alaska",
@@ -67,17 +67,29 @@ def generate_logs(count):
         dt = datetime.datetime.now().date().strftime("%m-%d-%Y")
         tm = time.strftime("%H:%M:%S")
         status = random.choice(["up", "up", "up", "up", "up", "up", "up", "up", "up", "down"])
-        province = random.choice(states)
+        state = random.choice(states)
 
         date_time = "{} {}" . format (dt, tm)
-    	message = "{} - {}: {}" . format (date_time, status, province)
+    	message = "{} - {}: {}" . format (date_time, status, state)
     	
+    	data = {
+    		"date_time": date_time,
+    		"date": dt,
+    		"time": tm,
+    		"status": status,
+    		"state": state
+    	}
+
+    	response = requests.post("http://104.236.214.67:8201/incoming/statusdata", json=data)
+    	print(response.content)
+
+    	"""
     	with open("logstash/inputs/events.log", "a") as log_file:
     		log_file.write("{}\n" . format(message))
     		wait_period = random.choice(range(0,10))
     		print(".")
     		time.sleep(wait_period)
-
+    	"""
 
 if __name__ == '__main__':
     generate_logs()
